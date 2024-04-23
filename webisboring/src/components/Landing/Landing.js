0c517ae8b4524a0b83db76b06a4c91c5708c6a17
Toby <57487614+DZNpepper@users.noreply.github.com>
2024-04-24 00:27:40 +0100

    Landing update

--- webisboring/src/components/Landing/Landing.js
+++ webisboring/src/components/Landing/Landing.js
@@ -1,10 +1,9 @@
-// React
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 // Components
 import Title from '../Title/Title';
-//Images
-import Image from '../../assets/images/Ciruit.webp';
+// Images
+import Image from '../../assets/images/Case3.webp';
 // Styling
 import './Style.scss';
 
@@ -13,20 +12,11 @@ const Landing = () => {
 
   return (
     <div className="landing-page">
-      {/* Title and Description */}
       <Title text="The Web is Boring" />
-      <p>Discover the components of computers. <br /><br /><br /> Select the first component to learn how to build a PC.</p>
-
-      {/* Scroll Down Indicator */}
-      <div className="scroll-down">⟱ Swipe down ⟱</div>
-
-      {/* Image */}
-      <img src={Image} alt="Circuit board" className="landing-image" />
-
-      {/* Content for entering the site */}
+      <p>Discover the components of computers. Select the first component to learn how to build a PC.</p>
+      <img src={Image} alt="Computer Case" className="landing-image" />
       <div className="enter-site">
         <p>Ready to explore how a computer is built? Click below to begin your journey!</p>
-        {/* Button for navigation */}
         <button onClick={() => navigate('/main-page')}>Explore</button>
       </div>
     </div>
--- webisboring/src/components/PSU/Description.js
+++ webisboring/src/components/PSU/Description.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Description = () => {
     return (
         <div className="psu-description">
-            <h2>Power Supply Unit (PSU) Description</h2>
+            <h2>Power Supply Unit (PSU) </h2>
             <p>The PSU in a computer converts the power from the wall outlet into usable power for the other components of the computer.</p>
             <p>It converts AC to low-voltage regulated DC power for the internal components of a computer.</p>
             <p>A reliable PSU is crucial because it provides consistent power to all the components and ensures their stability and longevity.</p>
